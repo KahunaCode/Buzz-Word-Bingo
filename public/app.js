62ac325d4df7dc89258aa72fdfaef95c0db7b805
James Moore <jameslmoore@gmail.com>
2017-07-12 21:39:01 -1000

    server works

--- public/app.js
+++ public/app.js
@@ -1,3 +1,15 @@
 /* jshint esversion: 6 */
 
 const express = require('express');
+const app = express();
+
+
+
+
+
+const server = app.listen(3000, () => {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log(`server running on port ${port}`);
+});
