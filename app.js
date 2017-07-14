e4f715d8056a0d2d2caaabcc9656224ce30e0e96
James Moore <jameslmoore@gmail.com>
2017-07-13 21:59:55 -1000

    validation

--- app.js
+++ app.js
@@ -17,42 +17,60 @@ app.get('/buzzwords', (req,res) => {
 });
 
 app.post('/buzzword', (req,res) => {
-  bw.push(req.body);
-  console.log('bw is', bw);
-  res.send({"success":true});
+  if (typeof req.body.buzzWord === "string" && typeof req.body.points === "number" && typeof req.body.heard === "boolean" ) {
+    bw.push(req.body);
+    console.log('bw is', bw);
+    res.send(`{"success":true}`);
+  }
+  else {
+    console.log("user = stupid. sent wrong thing");
+    res.send(`{"success":false}`);
+  }
 });
 
 app.put('/buzzword', (req,res) => {
   console.log("put up or shut up");
-  bw.forEach((el) => {
-    if (el.buzzWord === req.body.buzzWord) {
-      el.points = req.body.points;
-      console.log('bw is now', bw);
-      res.end(`{ "success": true, newScore: ${el.points} }`);
-    }
-  });
+  if (typeof req.body.buzzWord === "string" && typeof req.body.points === "number" && typeof req.body.heard === "boolean") {
+    bw.forEach((el) => {
+      if (el.buzzWord === req.body.buzzWord) {
+        el.points = req.body.points;
+        console.log('bw is now', bw);
+        res.end(`{ "success": true, newScore: ${el.points} }`);
+      }
+    });
+  }
+  else{
+    console.log("user = stupid. sent wrong thing");
+    res.send(`{"success":false}`);
+  }
   res.end(`{"success": false}`);
 });
 
 app.delete('/buzzword', (req, res) => {
   console.log("deleteme");
-  bw.forEach((el) => {
-    if (el.buzzWord === req.body.buzzWord) {
-      var idx = bw.indexOf(req.body.buzzWord);
-      bw.splice(idx, 1);
-      console.log('bw is now', bw);
-      res.end(`{"success": true}`);
-    }
-  });
+  if (typeof req.body.buzzWord === "string" && typeof req.body.points === "number" && typeof req.body.heard === "boolean") {
+    bw.forEach((el) => {
+      if (el.buzzWord === req.body.buzzWord) {
+        var idx = bw.indexOf(req.body.buzzWord);
+        bw.splice(idx, 1);
+        console.log('bw is now', bw);
+        res.end(`{"success": true}`);
+      }
+    });
+  }
+  else{
+    console.log("user = stupid. sent wrong thing");
+    res.send(`{"success":false}`);
+  }
   res.end(`{"success" : false}`);
 });
 
 app.post('/reset', (req, res) => {
-  if (req.body.reset === true) {
-    bw = [];
-    console.log('bw deleted', bw);
-    res.end(`{"success": true}`);
-  }
+    if (req.body.reset === true) {
+      bw = [];
+      console.log('bw deleted', bw);
+      res.end(`{"success": true}`);
+    }
 });
 
 const server = app.listen(3000, () => {
