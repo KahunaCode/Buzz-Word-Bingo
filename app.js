80932de04ae2f3c57aa7da46fed2d8b632ffbc4c
James Moore <jameslmoore@gmail.com>
2017-07-13 18:27:25 -1000

    post

--- app.js
+++ app.js
@@ -3,32 +3,38 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const bp = require('body-parser');
+
+app.use(bp());
 
 app.use(express.static('public'));
 
+// router.route('/buzzwords')
+//   .get((req,res) => {
+//     console.log("buzzwords here");
+//     res.send("buzzwords JSON thing should be sent");
+//     res.end();
+//   })
+//   .post((req,res) => {
+//     console.log("got buzzwords POST");
+//     res.send({"success":true});
+//   });
 
-router.route('/')
-  .get((req,res) => {
-    console.log("buzzwords here");
-    res.send("buzzwords JSON thing should be sent");
-  })
-  .post((req,res) => {
-    console.log("got buzzwords POST");
-    res.send({"success":true});
-  });
+var bw = {};
 
 
+app.get('/buzzwords', (req,res) => {
+  console.log("buzzwords here");
+  res.send("buzzwords JSON thing should be sent");
+});
 
-
-// app.get('/buzzwords', (req,res) => {
-//   console.log("buzzwords here");
-//   res.send("buzzwords JSON thing should be sent");
-// });
-
-// app.post('/buzzwords', (req,res) => {
-//   console.log("got buzzwords POST");
-//   res.send({"success":true});
-// });
+app.post('/buzzwords', (req,res) => {
+  //console.log(req.body);
+  bw[req.body.buzzWord] = req.body.points;
+  // console.log('bw is', bw);
+  // console.log("got buzzwords POST");
+  res.send({"success":true});
+});
 
 const server = app.listen(3000, () => {
   var host = server.address().address;
